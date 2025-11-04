4bff98ace6780c8f9cea0a754e34eb19b32d4285
Tarcísio Alves de Araújo Filho <104470698+tadaf18@users.noreply.github.com>
2025-11-04 17:40:14 -0300

    dashboards.py

--- dashboards.py
+++ dashboards.py
@@ -5,6 +5,4 @@ def run():
 
     st.write("Aqui estão alguns dos dashboards que criei para análise de dados. Cada link leva a um dashboard interativo que você pode explorar.")
 
-    st.write("[Dashboard 1 - Análise da PNAD Contínua IBGE - Power BI](https://app.powerbi.com/view?r=eyJrIjoiZjFiZGE2MzctMmUzYS00YWRiLWJhZDAtODUyMDVlMDZlMDBmIiwidCI6IjI3OTg4MjBhLWI2OTMtNDcxOS1hNzA2LTI5ODRhZDI2OTY5YiJ9&pageName=ReportSectiona033732d63748cee00a3)")
-    st.write("[Dashboard 2 - Dashboard Brasileirão 2024 - Python](https://appbrasileirao2024.streamlit.app/)")
-    st.write("[Dashboard 3 - Dashboard Brasileirão - Power BI](https://app.powerbi.com/view?r=eyJrIjoiMzEwMzAzZjktNTIyYi00MmRkLWE3MjQtZGUyYWI5OTg4ODJjIiwidCI6IjI3OTg4MjBhLWI2OTMtNDcxOS1hNzA2LTI5ODRhZDI2OTY5YiJ9&pageName=ReportSectionf8622281a148deb6bed9)")
+    st.write("[Dashboard 1 - Power BI]()
